56522176d673bf0c38bd79c7801d11510112b61e
TestofficialXD <mrlizard1288@gmail.com>
2024-10-09 00:13:29 -0400

    Update Esp.lua

--- Esp.lua
+++ Esp.lua
@@ -1,1 +1,1 @@
-loadstring("https://raw.githubusercontent.com/TestofficialXD/Roblox-Scripts/refs/heads/main/ESPRAW.lua")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TestofficialXD/Roblox-Scripts/refs/heads/main/ESPRAW.lua")
